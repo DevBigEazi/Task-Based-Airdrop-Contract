1ad53d3e10915d9add40866275e885a7ce3f3295
devbigeazi <adesholatajudeen1@gmail.com>
2024-10-30 13:37:52 +0100

    Completed main contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,14 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyTokenTask is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC20("MyTask", "MTK")
-        Ownable(initialOwner)
-    {}
+    constructor(
+        address initialOwner
+    ) ERC20("Roqisiq", "RQ") Ownable(initialOwner) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
-
 }
